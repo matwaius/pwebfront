e46c3431a257ac8ebfc153d68afafcf3405e7d5a
matwaius <marcuswalves@gmail.com>
2019-04-17 21:56:36 -0300

    FRONT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nome = 'Marcus';
+  categorias =[
+      {id:"1",nome:"Categoria 1"},
+      {id:"2",nome:"Categoria 2"},
+      {id:"3",nome:"Categoria 3"},
+      {id:"4",nome:"Categoria 4"},
+      {id:"5",nome:"Categoria 5"},
+      {id:"6",nome:"Categoria 6"},
+      {id:"7",nome:"Categoria 7"},
+      {id:"8",nome:"Categoria 8"},
+      {id:"9",nome:"Categoria 9"},
+      {id:"10",nome:"Categoria 10"}
+    ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {TooltipModule} from 'primeng/tooltip';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
